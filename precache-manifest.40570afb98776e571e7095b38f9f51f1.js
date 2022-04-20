286eaf318ceceb9df96f33094f9a769eff2f87a2
Darwin Perdomo <ZeroBl@hotmail.com>
2022-04-19 21:37:00 -0400

    Updates

---
+++ precache-manifest.40570afb98776e571e7095b38f9f51f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8381b32124d99e8a9851a33e6c590d02",
+    "url": "/memories-app/index.html"
+  },
+  {
+    "revision": "5a31b0efd4513c5d4e72",
+    "url": "/memories-app/static/css/main.2316c888.chunk.css"
+  },
+  {
+    "revision": "bebe2336014060f4d51d",
+    "url": "/memories-app/static/js/2.9f3cc2f9.chunk.js"
+  },
+  {
+    "revision": "cd94619111e2ab78ce54eac27b302772",
+    "url": "/memories-app/static/js/2.9f3cc2f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a31b0efd4513c5d4e72",
+    "url": "/memories-app/static/js/main.69ff324a.chunk.js"
+  },
+  {
+    "revision": "0a16476c4360cd00b683",
+    "url": "/memories-app/static/js/runtime-main.6ee3edb2.js"
+  },
+  {
+    "revision": "4b9dc077a5e83bc17478662d0144dc92",
+    "url": "/memories-app/static/media/memories-Logo.4b9dc077.png"
+  },
+  {
+    "revision": "18ebdb6d48b70ab2e72ac3bf9154d8be",
+    "url": "/memories-app/static/media/memories-Text.18ebdb6d.png"
+  }
+]);
